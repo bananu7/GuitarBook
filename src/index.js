985175511c5d5024434948df207f57a0a011cea8
bananu7 <bananu7@o2.pl>
2016-07-05 15:01:56 +0200

    Added a draft of page processing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-function go() {
-    $.get('pages/intervals.html', function (data) {
-        $(document.body).append($(data));
+function addPage(url) {
+    $.get(url, function (data) {
+        var page = $(data);
+        processPageData(page);
+        $(document.body).append(page);
     });
+}
+
+// TEMP TODO
+function processPageData(pageElement) {
+    $(pageElement).find('fretboard').replaceWith(function(f) {
+        return $('<span>fretboard diagram</span>');
+    });
+}
+
+function go() {
+    ['pages/intervals.html'
+    ,'pages/fretboard.html'].forEach(addPage);
 }
