45a7e9a402558cf116a72438058ce4291e35bfe4
bananu7 <bananu7@o2.pl>
2017-11-08 21:39:45 +0100

    Removed unnecessary logging

--- src/js/misc.js
+++ src/js/misc.js
@@ -7,19 +7,16 @@ function renderIntervalBlocks(pageElement) {
 }
 
 function createIntervalBlocks(elem) {
-    console.log('c i b');
     const list = document.createElement('div');
     list.className += 'interval-list';
 
     let num = 1;
     $(elem).text().split('').forEach(interval => {
-        console.log(interval);
         const block = document.createElement('span');
         block.className += interval;
         block.innerText = num++;
         list.append(block);
     });
 
-    console.log(list);
     return list;
 }
